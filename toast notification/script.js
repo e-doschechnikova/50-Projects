64f86f5282e537bd60a0a948db640a6ea61bcc5f
e-doschechnikova <evadrobakova@gmail.com>
2022-01-28 15:24:06 +0300

    add toast notification

---
+++ toast notification/script.js
@@ -0,0 +1,30 @@
+const button = document.getElementById("button");
+const toasts = document.getElementById("toasts");
+
+const messages = ["Hello!", "How are you?", "Where are you from?", "Goodbye!"];
+
+const types = ["info", "succes", "error"];
+
+button.addEventListener("click", () => createNotification());
+
+function createNotification(message = null, type = null) {
+  const notif = document.createElement("div");
+  notif.classList.add("toast");
+  notif.classList.add(type ? type : getRandomType());
+
+  notif.innerText = message ? message : getRandomMessage();
+
+  toasts.appendChild(notif);
+
+  setTimeout(() => {
+    notif.remove();
+  }, 3000);
+}
+
+function getRandomMessage() {
+  return messages[Math.floor(Math.random() * messages.length)];
+}
+
+function getRandomType() {
+  return types[Math.floor(Math.random() * messages.length)];
+}
